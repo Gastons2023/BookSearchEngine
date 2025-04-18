5939caf73589a8963055cb89e75cd40dabf76198
Gastons2023 <kechara.neal@gmail.com>
2025-04-18 09:11:10 -0400

    set up appollo server

---
+++ server/src/schema/resolvers.ts
@@ -0,0 +1,13 @@
+
+
+const resolvers = {
+  Query: {
+    me: () => {
+      // Placeholder for the 'me' query resolver
+      return null; // Replace with actual logic to fetch the current user
+    },
+  },
+  Mutation: {},
+};
+
+export default resolvers;
---
+++ server/src/server.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import db from './config/connection.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import http from 'http';
+import { typeDefs, resolvers } from './schema/index.js';
+
+interface MyContext {
+  token?: String;
+}
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const httpServer = http.createServer(app);
+const server = new ApolloServer<MyContext>({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use((_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+app.use('/graphql',
+  express.urlencoded({ extended: true }),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
+
+db.once('open', () => {
+  console.log('🚀 MongoDB connection established');
+});
+
+await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve));
+console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+
+
+
+
+
+
+
